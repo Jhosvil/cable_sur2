640ed806ab124d8162e7f3c3692230b9d1bb7ad1
Smith <smith@gmail.com>
2022-11-22 14:07:18 -0500

    pruebas para subir al hostinger3

--- config/database.php
+++ config/database.php
@@ -6,6 +6,6 @@ define("USER", "u261288288_cable_sur_user");
 define("PASS", "Smith71944332");
 define("DATABASE", "u261288288_cable_sur");
 define("PORT", "3306");
-define("CHARSET", "utf8mb4_general_ci");
+define("CHARSET", "utf8mb4");
 
 ?>
